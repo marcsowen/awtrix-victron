024bdb7daeb48b4781102f6515d87a6fa1f9cee9
Marc Sowen <marc@sowen.net>
2025-01-31 09:23:37 +0100

    Colored bar chart

--- awtrix-victron.py
+++ awtrix-victron.py
@@ -17,13 +17,7 @@ def send_to_awtrix(ip, data):
     bat_soc = data["bat_soc"]
     bat_soc_icon = 6354 + int(bat_soc / 25)
 
-    price = round(data["evu_price"], 2)
-    if price < 0.30:
-        price_icon = 3961 # green
-    elif 0.30 <= price < 0.40:
-        price_icon = 6256 # yellow
-    else:
-        price_icon = 3813 # red
+    price_icon = data["evu_price"]["icon"]
 
     temperature = data["temperature"]
     temperature_icon = 21750 - max(min(int((temperature + 20) / 10), 5), 0)
@@ -46,13 +40,12 @@ def send_to_awtrix(ip, data):
         },
         {
             "icon": price_icon,
-            "text": "%.2f" % price,
+            "text": "%.2f" % data["evu_price"]["price"],
             "lifetime": 300
         },
         {
             "icon": price_icon,
-            "bar": data["evu_price_bars"],
-            "autoscale": False,
+            "draw": data["evu_price"]["bars"],
             "lifetime": 300
         },
         {
@@ -90,14 +83,17 @@ def get_energy_price():
     index = response["unix_seconds"].index(current_hour_timestamp)
     end_index = min(len(response["unix_seconds"]) - index, 11) + index
     current_price = get_evu_price_in_euro(response["price"][index])
-    bar_chart = response["price"][index:end_index]
-    bar_chart_min_value = min(bar_chart)
-    bar_chart_max_value = max(bar_chart)
-    bar_chart_int = [int(round((((value - bar_chart_min_value) / (bar_chart_max_value - bar_chart_min_value)) * 7) + 1, 0)) for value in bar_chart]
+    bar_chart_stock = response["price"][index:end_index]
+    bar_chart_min_value = min(bar_chart_stock)
+    bar_chart_max_value = max(bar_chart_stock)
+    bar_chart_int = [int(round((((value - bar_chart_min_value) / (bar_chart_max_value - bar_chart_min_value)) * 7) + 1, 0)) for value in bar_chart_stock]
+    bar_chart_euro = [get_evu_price_in_euro(price) for price in bar_chart_stock]
+    bar_chart_color = [get_color_from_price(price) for price in bar_chart_euro]
 
     result = {
-        "evu_price": current_price,
-        "evu_price_bars": bar_chart_int
+        "price": current_price,
+        "icon": get_color_from_price(current_price)["icon"],
+        "bars": get_bar_graph_drawing(bar_chart_int, bar_chart_color),
     }
 
     g_price_last_timestamp = current_hour_timestamp
@@ -105,15 +101,35 @@ def get_energy_price():
 
     return result
 
+def get_bar_graph_drawing(heights, colors) -> list:
+    data_size = len(heights)
+    start_x = 9
+    result = []
+
+    for i in range(0, data_size):
+        x = start_x + i * 2
+        y = 8 - heights[i]
+        result.append({"df": [x, y, 1, heights[i], colors[i]["color"]]})
+
+    return result
+
+def get_color_from_price(price: float) -> dict:
+    if price < 0.30:
+        return { "color": "#8aff0f", "icon": 3961} # green
+    elif 0.30 <= price < 0.40:
+        return { "color": "#f7f443", "icon": 6256} # yellow
+    else:
+        return { "color": "#f70016", "icon": 3813} # red
+
 def get_evu_price_in_euro(stock_price: float) -> float:
-    return (stock_price / 1000 * 1.19) + 0.1978 # Green Planet Energy Ökostrom flex (since 01/2025)
+    return round((stock_price / 1000 * 1.19) + 0.1978, 2) # Green Planet Energy Ökostrom flex (since 01/2025)
 
 def get_outside_weather(ip: str, ble_mac: str):
     response = json.loads(requests.get("http://" + ip).content.decode('UTF-8'))
     return next(sensor for sensor in response["sensors"] if sensor["ble_mac"] == ble_mac)
 
 def main():
-    print("awtrix-victron v1.1")
+    print("awtrix-victron v1.2")
     victron_ip = "192.168.178.104"
     awtrix_ip = "192.168.178.143"
     weather_sensor_ip = "192.168.178.157"
@@ -139,8 +155,7 @@ def main():
             "ac_power": l1 + l2 + l3,
             "pv_power": pv_p,
             "bat_soc": soc,
-            "evu_price": energy_price["evu_price"],
-            "evu_price_bars": energy_price["evu_price_bars"],
+            "evu_price": energy_price,
             "temperature": weather["temperature"]
         }
 
